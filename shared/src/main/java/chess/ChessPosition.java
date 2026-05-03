ea1c59c4e18a1a93dd74b61afd3a505911177ea0
Josh <joshbbrown1007@gmail.com>
2026-05-03 12:25:37 -0600

    Implemented toString in chessmove

--- shared/src/main/java/chess/ChessPosition.java
+++ shared/src/main/java/chess/ChessPosition.java
@@ -32,7 +32,8 @@ public class ChessPosition {
         return col;
     }
 
+    @Override
     public String toString() {
-        return "ChessPosition{" + "row: " + row + ", col: " + col + "}";
+        return "[" + row + "," + col + "]";
     }
 }
