98751af059a2452337f9a509cf8991a5892fa0f8
Josh <joshbbrown1007@gmail.com>
2026-05-18 16:40:41 -0600

    Added methods to return all of the values in GameData

--- shared/src/main/java/model/GameData.java
+++ shared/src/main/java/model/GameData.java
@@ -18,5 +18,31 @@ public class GameData {
         this.gameName = gameName;
         this.game = game;
     }
+
+    //gets the game ID
+    public int getGameID() {
+        return gameID;
+    }
+
+    //gets the white user
+    public String getWhite() {
+        return whiteUsername;
+    }
+
+    //gets the black user
+    public String getBlack() {
+        return blackUsername;
+    }
+
+    //gets the game name
+    public String getName() {
+        return gameName;
+    }
+
+    //returns the game object
+    public ChessGame getGame() {
+        return game;
+    }
+
     
 }
