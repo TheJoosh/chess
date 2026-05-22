da61751d13028237d9b13b874f47aeec352878e7
Josh <joshbbrown1007@gmail.com>
2026-05-22 16:01:26 -0600

    Edited code for quality

--- server/src/test/java/service/ServiceTests.java
+++ server/src/test/java/service/ServiceTests.java
@@ -15,21 +15,21 @@ import results.LoginRequest;
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class ServiceTests {
-    static final AuthService authService = new AuthService(new AuthAccess());
-    static final UserService userService = new UserService(new UserAccess());
-    static final GameService gameService = new GameService(new GameAccess());
+    static final AuthService AUTH_SERVICE = new AuthService(new AuthAccess());
+    static final UserService USER_SERVICE = new UserService(new UserAccess());
+    static final GameService GAME_SERVICE = new GameService(new GameAccess());
 
     @BeforeEach
     void clearData() throws DataAccessException {
-        authService.clear();
-        userService.clear();
-        gameService.clear();
+        AUTH_SERVICE.clear();
+        USER_SERVICE.clear();
+        GAME_SERVICE.clear();
     }
 
     @Test
     void registerTestPositive() throws DataAccessException {
         var user = new UserData("username", "password", "email");
-        AuthData auth = userService.register(user);
+        AuthData auth = USER_SERVICE.register(user);
         
         assertEquals("username", auth.username());
     }
@@ -40,77 +40,77 @@ public class ServiceTests {
         var nullPassword = new UserData("username", null, "gmail");
         var nullEmail = new UserData("yusername", "password", null);
         
-        assertThrows(DataAccessException.class, () -> userService.register(nullName));
-        assertThrows(DataAccessException.class, () -> userService.register(nullPassword));
-        assertThrows(DataAccessException.class, () -> userService.register(nullEmail));
-        assertThrows(DataAccessException.class, () -> userService.register(null));
+        assertThrows(DataAccessException.class, () -> USER_SERVICE.register(nullName));
+        assertThrows(DataAccessException.class, () -> USER_SERVICE.register(nullPassword));
+        assertThrows(DataAccessException.class, () -> USER_SERVICE.register(nullEmail));
+        assertThrows(DataAccessException.class, () -> USER_SERVICE.register(null));
     }
 
     @Test
     void getUserTestPositive() throws DataAccessException {
-        userService.register(new UserData("username", "password", "email"));
-        boolean gotUser = userService.getUser(new UserData("username", "password", "email"));
+        USER_SERVICE.register(new UserData("username", "password", "email"));
+        boolean gotUser = USER_SERVICE.getUser(new UserData("username", "password", "email"));
 
         assert(gotUser);
     }
 
     @Test
     void getUserTestNegative() throws DataAccessException {
-        userService.register(new UserData("username", "password", "email"));
-        boolean gotUser = userService.getUser(new UserData("yusername", "password", "email"));
+        USER_SERVICE.register(new UserData("username", "password", "email"));
+        boolean gotUser = USER_SERVICE.getUser(new UserData("yusername", "password", "email"));
 
         assert(!gotUser);
     }
 
     @Test
     void verifyPasswordTestPositive() throws DataAccessException {
-        userService.register(new UserData("username", "password", "email"));
+        USER_SERVICE.register(new UserData("username", "password", "email"));
 
-        boolean verified = userService.verifyPassword(new LoginRequest("username", "password"));
+        boolean verified = USER_SERVICE.verifyPassword(new LoginRequest("username", "password"));
         assert(verified);
     }
 
     @Test
     void verifyPasswordTestNegative() throws DataAccessException {
-        userService.register(new UserData("username", "password", "email"));
+        USER_SERVICE.register(new UserData("username", "password", "email"));
 
-        boolean verified1 = userService.verifyPassword(new LoginRequest("yusername", "password"));
-        boolean verified2 = userService.verifyPassword(new LoginRequest("username", "pasword"));
+        boolean verified1 = USER_SERVICE.verifyPassword(new LoginRequest("yusername", "password"));
+        boolean verified2 = USER_SERVICE.verifyPassword(new LoginRequest("username", "pasword"));
         assert(!verified1 && !verified2);
     }
 
     @Test
     void listUsersTest() throws DataAccessException {
-        userService.register(new UserData("username", "password", "email"));
-        userService.register(new UserData("yusername", "password", "gmail"));
+        USER_SERVICE.register(new UserData("username", "password", "email"));
+        USER_SERVICE.register(new UserData("yusername", "password", "gmail"));
 
-        assertEquals(2, userService.listUsers().size());
+        assertEquals(2, USER_SERVICE.listUsers().size());
     }
 
     @Test
     void listUsersTestEmpty() throws DataAccessException {
-        assertEquals(0, userService.listUsers().size());
+        assertEquals(0, USER_SERVICE.listUsers().size());
     }
 
     @Test
     void authenticateTestPositive() throws DataAccessException {
-        AuthData auth = userService.register(new UserData("username", "password", "email"));
-        authService.addAuth(auth);
+        AuthData auth = USER_SERVICE.register(new UserData("username", "password", "email"));
+        AUTH_SERVICE.addAuth(auth);
 
-        assert(authService.authenticate(auth.authToken()));
+        assert(AUTH_SERVICE.authenticate(auth.authToken()));
     }
 
     @Test
     void authenticateTestNegative() throws DataAccessException {
-        assert(!authService.authenticate(UUID.randomUUID().toString()));
+        assert(!AUTH_SERVICE.authenticate(UUID.randomUUID().toString()));
     }
 
     @Test
     void addAuthPositive() throws DataAccessException {
         String authToken = UUID.randomUUID().toString();
-        authService.addAuth(new AuthData(authToken, "username"));
+        AUTH_SERVICE.addAuth(new AuthData(authToken, "username"));
 
-        assert(authService.listAuth().contains("username"));
+        assert(AUTH_SERVICE.listAuth().contains("username"));
     }
 
     @Test
@@ -119,64 +119,64 @@ public class ServiceTests {
         String authToken = UUID.randomUUID().toString();
         AuthData nullName = new AuthData(authToken, null);
 
-        assertThrows(DataAccessException.class, () -> authService.addAuth(nullToken));
-        assertThrows(DataAccessException.class, () -> authService.addAuth(nullName));
-        assertThrows(DataAccessException.class, () -> authService.addAuth(null));
+        assertThrows(DataAccessException.class, () -> AUTH_SERVICE.addAuth(nullToken));
+        assertThrows(DataAccessException.class, () -> AUTH_SERVICE.addAuth(nullName));
+        assertThrows(DataAccessException.class, () -> AUTH_SERVICE.addAuth(null));
     }
 
     @Test
     void removeAuthPositive() throws DataAccessException {
         String authToken = UUID.randomUUID().toString();
-        authService.addAuth(new AuthData(authToken, "username"));
-        assert(authService.listAuth().contains("username"));
+        AUTH_SERVICE.addAuth(new AuthData(authToken, "username"));
+        assert(AUTH_SERVICE.listAuth().contains("username"));
 
-        authService.removeAuth(authToken);
+        AUTH_SERVICE.removeAuth(authToken);
 
-        assert(!authService.listAuth().contains("username"));
+        assert(!AUTH_SERVICE.listAuth().contains("username"));
     }
 
     @Test
     void removeAuthNegative() throws DataAccessException {
-        assertThrows(DataAccessException.class, () -> authService.removeAuth(UUID.randomUUID().toString()));
+        assertThrows(DataAccessException.class, () -> AUTH_SERVICE.removeAuth(UUID.randomUUID().toString()));
     }
 
     @Test
     void listAuthTest() throws DataAccessException {
-        authService.addAuth(new AuthData(UUID.randomUUID().toString(), "username"));
-        authService.addAuth(new AuthData(UUID.randomUUID().toString(), "yusername"));
+        AUTH_SERVICE.addAuth(new AuthData(UUID.randomUUID().toString(), "username"));
+        AUTH_SERVICE.addAuth(new AuthData(UUID.randomUUID().toString(), "yusername"));
 
-        assertEquals(2, authService.listAuth().size());
+        assertEquals(2, AUTH_SERVICE.listAuth().size());
     }
 
     @Test
     void listAuthTestEmpty() throws DataAccessException {
-        assertEquals(0, authService.listAuth().size());
+        assertEquals(0, AUTH_SERVICE.listAuth().size());
     }
 
     @Test
     void loginTestPositive() throws DataAccessException {
-        userService.register(new UserData("username", "password", "email"));
+        USER_SERVICE.register(new UserData("username", "password", "email"));
         var user = new LoginRequest("username", "password");
-        AuthData auth = authService.login(user);
+        AuthData auth = AUTH_SERVICE.login(user);
         
         assertEquals("username", auth.username());
     }
 
     @Test
-    void LoginTestNegative() throws DataAccessException {
+    void loginTestNegative() throws DataAccessException {
         var nullName = new LoginRequest(null, "password");
         var nullPassword = new LoginRequest("username", null);
         
-        assertThrows(DataAccessException.class, () -> authService.login(nullName));
-        assertThrows(DataAccessException.class, () -> authService.login(nullPassword));
-        assertThrows(DataAccessException.class, () -> authService.login(null));
+        assertThrows(DataAccessException.class, () -> AUTH_SERVICE.login(nullName));
+        assertThrows(DataAccessException.class, () -> AUTH_SERVICE.login(nullPassword));
+        assertThrows(DataAccessException.class, () -> AUTH_SERVICE.login(null));
     }
 
     @Test
     void getUsernameTestPositive() throws DataAccessException {
-        AuthData auth = userService.register(new UserData("username", "password", "email"));
-        authService.addAuth(auth);
-        String username = authService.getUsername(auth.authToken());
+        AuthData auth = USER_SERVICE.register(new UserData("username", "password", "email"));
+        AUTH_SERVICE.addAuth(auth);
+        String username = AUTH_SERVICE.getUsername(auth.authToken());
 
         assertEquals(username, "username");
     }
@@ -184,70 +184,70 @@ public class ServiceTests {
     @Test
     void getUsernameTestNegative() throws DataAccessException {
 
-        assertThrows(DataAccessException.class, () -> authService.getUsername(null));
+        assertThrows(DataAccessException.class, () -> AUTH_SERVICE.getUsername(null));
 
-        AuthData auth = userService.register(new UserData("username", "password", "email"));
-        authService.addAuth(auth);
-        assertThrows(DataAccessException.class, () -> authService.getUsername(UUID.randomUUID().toString()));
+        AuthData auth = USER_SERVICE.register(new UserData("username", "password", "email"));
+        AUTH_SERVICE.addAuth(auth);
+        assertThrows(DataAccessException.class, () -> AUTH_SERVICE.getUsername(UUID.randomUUID().toString()));
     }
 
     @Test
     void listGamesTest() throws DataAccessException {
-        gameService.createGame("game1");
-        gameService.createGame("game2");
+        GAME_SERVICE.createGame("game1");
+        GAME_SERVICE.createGame("game2");
 
-        assertEquals(2, gameService.listGames().size());
+        assertEquals(2, GAME_SERVICE.listGames().size());
     }
 
     @Test
     void listGamesTestEmpty() throws DataAccessException {
-        assertEquals(0, gameService.listGames().size());
+        assertEquals(0, GAME_SERVICE.listGames().size());
     }
 
     @Test
     void createGamePositive() throws DataAccessException {
-        gameService.createGame("game");
+        GAME_SERVICE.createGame("game");
         ListGameResult game = new ListGameResult(1, null, null, "game");
 
-        assert(gameService.listGames().contains(game));
+        assert(GAME_SERVICE.listGames().contains(game));
     }
 
     @Test
     void createGameNegative() throws DataAccessException {
-        assertThrows(DataAccessException.class, () -> gameService.createGame(null));
+        assertThrows(DataAccessException.class, () -> GAME_SERVICE.createGame(null));
     }
 
     @Test
     void joinGamePositive() throws DataAccessException {
-        int gameID = gameService.createGame("game");
+        int gameID = GAME_SERVICE.createGame("game");
 
-        assert(gameService.joinGame("username", ChessGame.TeamColor.BLACK, gameID));
+        assert(GAME_SERVICE.joinGame("username", ChessGame.TeamColor.BLACK, gameID));
     }
 
     @Test
     void joinGameNegative() throws DataAccessException {
-        int gameID = gameService.createGame("game");
+        int gameID = GAME_SERVICE.createGame("game");
 
-        assertThrows(DataAccessException.class, () -> gameService.joinGame(null, ChessGame.TeamColor.BLACK, gameID));
-        assertThrows(DataAccessException.class, () -> gameService.joinGame("username", null, gameID));
+        assertThrows(DataAccessException.class, () -> GAME_SERVICE.joinGame(null, ChessGame.TeamColor.BLACK, gameID));
+        assertThrows(DataAccessException.class, () -> GAME_SERVICE.joinGame("username", null, gameID));
 
-        gameService.joinGame("username", ChessGame.TeamColor.BLACK, gameID);
+        GAME_SERVICE.joinGame("username", ChessGame.TeamColor.BLACK, gameID);
 
-        assert(!gameService.joinGame("yusername", ChessGame.TeamColor.BLACK, gameID));
+        assert(!GAME_SERVICE.joinGame("yusername", ChessGame.TeamColor.BLACK, gameID));
     }
 
     @Test
     void clearTest() throws DataAccessException {
-        userService.register(new UserData("username", "password", "email"));
-        gameService.createGame(new ChessGame().toString());
-        authService.addAuth(new AuthData(UUID.randomUUID().toString(), "username"));
+        USER_SERVICE.register(new UserData("username", "password", "email"));
+        GAME_SERVICE.createGame(new ChessGame().toString());
+        AUTH_SERVICE.addAuth(new AuthData(UUID.randomUUID().toString(), "username"));
 
-        userService.clear();
-        gameService.clear();
-        authService.clear();
-        assertEquals(0, gameService.listGames().size());
-        assertEquals(0, userService.listUsers().size());
-        assertEquals(0, authService.listAuth().size());
+        USER_SERVICE.clear();
+        GAME_SERVICE.clear();
+        AUTH_SERVICE.clear();
+        assertEquals(0, GAME_SERVICE.listGames().size());
+        assertEquals(0, USER_SERVICE.listUsers().size());
+        assertEquals(0, AUTH_SERVICE.listAuth().size());
     }
 
 }
