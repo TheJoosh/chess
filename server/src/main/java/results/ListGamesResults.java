d138eb97f5830024bde8bbf97ab8c6a2928f7c10
Josh <joshbbrown1007@gmail.com>
2026-05-26 20:43:26 -0600

    Fixed a bug with ListGames

--- server/src/main/java/results/ListGamesResults.java
+++ server/src/main/java/results/ListGamesResults.java
@@ -2,12 +2,14 @@ package results;
 
 import java.util.List;
 import java.util.HashMap;
+import java.util.ArrayList;
 import model.ListGameResult;
 
 public class ListGamesResults {
     private List<ListGameResult> games;
 
     public ListGamesResults(HashMap<Integer, ListGameResult> games) {
+        this.games = new ArrayList<ListGameResult>();
         for (ListGameResult item : games.values()) {
             this.games.add(item);
         }
