b846d6ec8c63b15858b1c3e669efdd7196bb27c6
Josh <joshbbrown1007@gmail.com>
2026-06-01 23:52:36 -0700

    Created the architecture for PostLogin

--- client/src/main/java/client/PostLogin.java
+++ client/src/main/java/client/PostLogin.java
@@ -13,12 +13,11 @@ public class PostLogin {
     private final ServerFacade server;
 
     String url;
-    Scanner scanner;
+    boolean signedIn = true;
 
-    public PostLogin (Scanner scanner, String url) throws ResponseException {
+    public PostLogin (String url) throws ResponseException {
         server = new ServerFacade(url);
         this.url = url;
-        this.scanner = scanner;
     }
 
     public void run() {
@@ -26,7 +25,73 @@ public class PostLogin {
         System.out.print(help());
         System.out.println();
 
+        Scanner scanner = new Scanner(System.in);
+        var result = "";
+        while (!result.equals("Closing chess")) {
+            String line = scanner.nextLine();
+            System.out.println();
 
+            try {
+                result = eval(line);
+                System.out.print(result);
+                System.out.println();
+                if (!signedIn) {
+                    return;
+                }
+            } catch (Throwable e) {
+                var msg = e.toString();
+                System.out.print(msg + "\n\n");
+            }
+        }
+        System.out.println();
+    }
+
+    public String eval(String input) {
+        try {
+            String[] tokens = input.toLowerCase().split(" ");
+            String cmd = (tokens.length > 0) ? tokens[0] : "help";
+            String[] params = Arrays.copyOfRange(tokens, 1, tokens.length);
+            return switch (cmd) {
+                case "create" -> createGame(params);
+                case "list" -> listGames(params);
+                case "join" -> joinGame(params);
+                case "observe" -> observeGame(params);
+                case "clear" -> clear(params);
+                case "quit" -> "Closing chess";
+                default -> help();
+            };
+        } catch (ResponseException ex) {
+            return ex.getMessage();
+        }
+    }
+
+    public String createGame(String... params) {
+        return "Game created";
+    }
+
+    public String listGames(String... params) {
+        return "Games listed";
+    }
+
+    public String joinGame(String... params) {
+        return "Game joined";
+    }
+
+    public String observeGame(String... params) {
+        return "Game observed";
+    }
+
+    public String clear(String... params) throws ResponseException {
+        if (params.length == 0) {
+            try {
+                server.clear();
+                return "Data cleared\n";
+            } catch (Exception e) {
+                throw new ResponseException(ResponseException.Code.BadRequest, "Clear unsuccessful\n");
+            }
+        }
+
+        throw new ResponseException(ResponseException.Code.BadRequest, "Bad Input\n");
     }
 
     public String help() {
--- client/src/main/java/client/PreLogin.java
+++ client/src/main/java/client/PreLogin.java
@@ -13,6 +13,7 @@ public class PreLogin {
     private final ServerFacade server;
 
     String url;
+    boolean signedIn = false;
 
     public PreLogin (String url) throws ResponseException {
         server = new ServerFacade(url);
@@ -27,14 +28,22 @@ public class PreLogin {
 
         Scanner scanner = new Scanner(System.in);
         var result = "";
-        while (!result.equals("quit")) {
+        while (!result.equals("Closing chess")) {
             String line = scanner.nextLine();
             System.out.println();
 
             try {
-                result = eval(scanner, line);
+                result = eval(line);
                 System.out.print(result);
                 System.out.println();
+                if (signedIn) {
+                    try {
+                        new PostLogin(url).run();
+                        return;
+                    } catch (Throwable ex) {
+                        System.out.printf("Unable to sign in: %s%n", ex.getMessage());
+                    }
+                }
             } catch (Throwable e) {
                 var msg = e.toString();
                 System.out.print(msg + "\n\n");
@@ -43,16 +52,16 @@ public class PreLogin {
         System.out.println();
     }
 
-    public String eval(Scanner scanner, String input) {
+    public String eval(String input) {
         try {
             String[] tokens = input.toLowerCase().split(" ");
             String cmd = (tokens.length > 0) ? tokens[0] : "help";
             String[] params = Arrays.copyOfRange(tokens, 1, tokens.length);
             return switch (cmd) {
-                case "login" -> login(scanner, params);
-                case "register" -> register(scanner, params);
+                case "login" -> login(params);
+                case "register" -> register(params);
                 case "clear" -> clear(params);
-                case "quit" -> "quit";
+                case "quit" -> "Closing chess";
                 default -> help();
             };
         } catch (ResponseException ex) {
@@ -60,18 +69,13 @@ public class PreLogin {
         }
     }
 
-    public String login(Scanner scanner, String... params) throws ResponseException {
+    public String login(String... params) throws ResponseException {
         if (params.length == 2) {
             try {
                 LoginRequest request = new LoginRequest(params[0], params[1]);
                 server.login(request);
-                try {
-                    new PostLogin(scanner, url).run();
-                    return String.format("Signed in as %s.\n", params[0]);
-                } catch (Throwable ex) {
-                    System.out.printf("Unable to sign in: %s%n", ex.getMessage());
-                }
-                
+                signedIn = true;
+                return String.format("Signed in as %s\n", params[0]);
             } catch (Exception e) {
                 throw new ResponseException(ResponseException.Code.BadRequest, "Incorrect username or password\n");
             }
@@ -79,17 +83,13 @@ public class PreLogin {
         throw new ResponseException(ResponseException.Code.BadRequest, "Expected: <username> <password>\n");
     }
 
-    public String register(Scanner scanner, String... params) throws ResponseException {
+    public String register(String... params) throws ResponseException {
         if (params.length == 3) {
             try {
                 UserData request = new UserData(params[0], params[1], params[2]);
                 server.register(request);
-                try {
-                    new PostLogin(scanner, url).run();
-                    return String.format("Signed in as %s.\n", params[0]);
-                } catch (Throwable ex) {
-                    System.out.printf("Unable to sign in: %s%n", ex.getMessage());
-                }
+                signedIn = true;
+                return String.format("Signed in as %s\n", params[0]);
             } catch (Exception e) {
                 throw new ResponseException(ResponseException.Code.BadRequest, "Existing username or email\n");
             }
@@ -101,7 +101,7 @@ public class PreLogin {
         if (params.length == 0) {
             try {
                 server.clear();
-                return "Data Cleared\n";
+                return "Data cleared\n";
             } catch (Exception e) {
                 throw new ResponseException(ResponseException.Code.BadRequest, "Clear unsuccessful\n");
             }
